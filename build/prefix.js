dba28ae19876e73dd439b0ce7dadac7ef718912a
jms <jm@audio.d3ck.net>
2017-11-30 16:27:25 +0100

    added interactive prompts to prefix and scope

--- build/prefix.js
+++ build/prefix.js
@@ -1,27 +1,154 @@
+/* eslint-env node */
 var fs = require('fs');
-var path = require('path');
 var glob = require('glob');
 var util = require('./util');
-var args = require('minimist')(process.argv);
+var argv = require('minimist')(process.argv);
+var inquirer = require('inquirer');
+var prompt = inquirer.createPromptModule();
 
-var prefix = args.p || args.prefix || false;
+argv._.forEach(arg => {
+    const tokens = arg.split('=');
+    argv[tokens[0]] = tokens[1] || true;
+});
 
-if (!prefix) {
-    return console.log('No prefix defined');
+var prefixFromInput = argv.p || argv.prefix;
+const allFiles = [];
+
+if (argv.h || argv.help) {
+    console.log(`
+        usage:
+
+        prefix.js [-p{refix}=your_great_new_prefix][replace|append|cleanup]
+
+        example:
+
+        prefix.js // will guide you throught the process
+        prefix.js -p=xyz replace // will replace any existing prefix with xyz
+        prefix.js append // will prompt for a prefix to append to the current one
+
+        note:
+        
+        if you don't want prefix.js to prompt you for input you have to provide all input parameters
+    `);
+} else {
+    readAllFiles().then(startProcess);
 }
 
-glob('dist/**/*.css', (err, files) =>
-    files.forEach(file =>
-        util.read(file, data =>
-            util.write(file, data.replace(/uk-([a-z\d\-]+)/g, `${prefix}-$1`))
-        )
-    )
-);
+function findExistingPrefix(data) {
+    const res = data.match(new RegExp(`(${util.validClassName.source})-grid`));
+    return res && res[1];
+}
 
-glob('dist/**/*.js', (err, files) =>
-    files.forEach(file =>
-        util.read(file, data =>
-            util.write(file, data.replace(/uk-/g, `${prefix}-`).replace(/UIkit/g, `${prefix}UIkit`))
+function getPrefix() {
+
+    if (!prefixFromInput) {
+        return prompt({name: 'prefix', message: 'enter a prefix', validate: (val, res) => val.length && val.match(util.validClassName) ? !!(res.prefix = val) : 'invalid prefix'}).then(res => res.prefix);
+    } else if (util.validClassName.test(prefixFromInput)) {
+        return Promise.resolve(prefixFromInput);
+    } else {
+        throw 'illegal prefix: ' + prefixFromInput;
+    }
+}
+
+function readAllFiles(prefix) {
+
+    const globs = [];
+
+    globs.push(new Promise(res =>
+        glob('dist/**/*.css', (err, files) => {
+            var reads = [];
+            files.forEach(file =>
+                reads.push(util.read(file, data =>
+                    allFiles.push({
+                        file,
+                        data,
+                        replace: (data, needle, replace) => data.replace(new RegExp(`${needle}-` + /([a-z\d\-]+)/.source, 'g'), `${replace}-$1`)
+                    })
+                ))
+            );
+            Promise.all(reads).then(res);
+        }
         )
-    )
-);
+    ));
+
+    globs.push(new Promise(res =>
+        glob('dist/**/*.js', (err, files) => {
+            var reads = [];
+            files.forEach(file =>
+                reads.push(util.read(file, data =>
+                    allFiles.push({
+                        file,
+                        data,
+                        replace: (data, needle, replace) => data.replace(new RegExp(`${needle}-`, 'g'), `${replace}-`).replace(new RegExp(`(${needle})?UIkit`, 'g'), `${replace === 'uk' ? '' : replace}UIkit`)
+                    })
+                ))
+            );
+            Promise.all(reads).then(res);
+        }
+        )
+    ));
+
+    return Promise.all(globs);
+}
+
+function replacePrefix(from, to) {
+    allFiles.forEach(({file, data, replace}) => {
+        data = replace(data, from, to);
+        fs.writeFileSync(file, data);
+    });
+}
+
+function dispatchCommand(action, currentPrefix) {
+
+    switch (action) {
+    case 'cleanup':
+        replacePrefix(currentPrefix, 'uk');
+        break;
+    case 'replace':
+        getPrefix().then(prefix => replacePrefix(currentPrefix, prefix));
+        break;
+    case 'append':
+        getPrefix().then(prefix => replacePrefix(currentPrefix, currentPrefix + prefix));
+        break;
+    case 'nothing':
+    }
+
+}
+
+function startProcess() {
+
+    // find existing prefix
+    var currentPrefix;
+    allFiles.some(({file, data}) => {
+        currentPrefix = findExistingPrefix(data);
+        return currentPrefix;
+    });
+
+    if (currentPrefix !== 'uk') {
+
+        const actions = ['replace', 'append', 'cleanup', 'nothing'];
+
+        const cliActions = actions.reduce((actions, action) => {
+            if (argv[action]) {
+                actions.push(action);
+            }
+            return actions;
+        }, []);
+
+        if (cliActions.length === 1) {
+            dispatchCommand(cliActions[0], currentPrefix);
+        } if (cliActions.length > 1) {
+            throw `multiple actions found: ${cliActions.join(', ')}`;
+        } else {
+            prompt([{
+                type: 'list',
+                name: 'action',
+                message: `this build is already prefixed with: '${currentPrefix}'. what do you want to do?`,
+                default: 'replace',
+                choices: actions
+            }]).then(({action}) => dispatchCommand(action, currentPrefix));
+        }
+    } else {
+        dispatchCommand('replace', currentPrefix);
+    }
+}
--- build/scope.js
+++ build/scope.js
@@ -1,12 +1,54 @@
+/* eslint-env node */
 var glob = require('glob');
 var util = require('./util');
 var argv = require('minimist')(process.argv.slice(2));
+
+argv._.forEach(arg => {
+    const tokens = arg.split('=');
+    argv[tokens[0]] = tokens[1] || true;
+});
+
 const inquirer = require('inquirer');
 const prompt = inquirer.createPromptModule();
-var newScope = argv.scope || 'uk-scope';
 
 const currentScopeRegex = /\/\* scoped: ([^\*]*) \*\//;
 const currentScopeLegacyRegex = new RegExp('\.(uk-scope)');
+const allFiles = [];
+var currentScope;
+
+if (argv.h || argv.help) {
+    console.log(`
+        usage:
+
+        scope.js [-scope=your_great_new_scope_name][replace|append|cleanup]
+
+        example:
+
+        scope.js // will guide you throught the process
+        scope.js -scope=uk-scope replace // will replace any existing scope with uk-scope
+        scope.js append // will prompt for a scopename to append to the current one
+
+        note:
+
+        if you don't want scope.js to prompt you for input you have to provide all input parameters
+    `);
+} else {
+    readAllFiles().then(startProcess);
+}
+
+function getNewScope() {
+
+    const scopeFromInput = typeof argv.scope === 'string' && argv.scope;
+
+    if (!scopeFromInput) {
+        return prompt({name: 'scope', default: 'uk-scope', message: 'enter a scope-name', validate: (val, res) => val.length && val.match(util.validClassName) ? !!(res.prefix = val) : 'invalid scope-name'}).then(res => res.prefix);
+    } else if (util.validClassName.test(scopeFromInput)) {
+        return Promise.resolve(scopeFromInput);
+    } else {
+        throw 'illegal scope-name: ' + scopeFromInput;
+    }
+}
+
 function isScoped(data) {
     var varName = data.match(currentScopeRegex);
     if (varName) {
@@ -17,75 +59,112 @@ function isScoped(data) {
     return varName && varName[1];
 }
 
-const allFiles = [];
-var currentScope;
+function doScope(scopeFromInput) {
 
-function doScope({file, data}) {
-    return util.renderLess(`.${newScope} {\n${data}\n}`)
-            .then(output => ({
-                file,
-                data: `/* scoped: ${currentScope ? currentScope + ' ' + newScope : newScope} */` +
-                    output.replace(new RegExp(`.${newScope} ${/{(.|[\r\n])*?}/.source}`), '')
-                    .replace(new RegExp(`.${newScope} ${/\s((\.(uk-(drag|modal-page|offcanvas-page|offcanvas-flip)))|html)/.source}`, 'g'), '$1')
-            })
-            );
-}
+    if (currentScope === scopeFromInput) {
+        return Promise.reject('already scoped with:' + currentScope);
+    }
 
-function store({file, data}) {
-    return util.write(file, data).then(util.minify);
-}
+    const scopes = [];
+    allFiles.forEach(store => {
 
-function cleanUp({file, data}) {
-    const string = currentScope.split(' ').map(scope => `.${scope}`).join(' ');
-    data = data.replace(new RegExp(/ */.source + string + / ({[\s\S]*?})?/.source, 'g'), '').replace(new RegExp(currentScopeRegex.source, 'g'), '');
-    return Promise.resolve({file, data});
-}
-
-glob('dist/**/!(*.min).css', (err, files) => {
-
-    //read files, check scopes
-    const reads = [];
-    files.forEach(file => {
-        const promise = util.read(file, data => {
-            allFiles.push({file, data});
-            const scope = isScoped(data);
-            if (currentScope && scope !== currentScope) {
-                throw 'scopes used on current css differ.';
-            }
-            currentScope = scope;
-        });
-        reads.push(promise);
+        scopes.push(util.renderLess(`.${scopeFromInput} {\n${store.data}\n}`)
+                        .then(output =>
+                            store.data = `/* scoped: ${currentScope ? currentScope + ' ' + scopeFromInput : scopeFromInput} */` +
+                                    output.replace(new RegExp(`.${scopeFromInput} ${/{(.|[\r\n])*?}/.source}`), '')
+                                          .replace(new RegExp(`.${scopeFromInput} ${/\s((\.(uk-(drag|modal-page|offcanvas-page|offcanvas-flip)))|html)/.source}`, 'g'), '$1')
+                        )
+        );
     });
 
-    Promise.all(reads).then(() => {
+    return Promise.all(scopes);
 
-        if (currentScope) {
+}
+
+function store() {
+    const writes = [];
+    allFiles.forEach(({file, data}) => writes.push(util.write(file, data).then(util.minify)));
+    return Promise.all(writes);
+}
+
+function cleanUp(currentScope) {
+    allFiles.forEach((store) => {
+        const string = currentScope.split(' ').map(scope => `.${scope}`).join(' ');
+        store.data = store.data.replace(new RegExp(/ */.source + string + / ({[\s\S]*?})?/.source, 'g'), '') // replace classes
+                   .replace(new RegExp(currentScopeRegex.source, 'g'), ''); // remove scope comment
+    });
+
+    currentScope = null;
+    return Promise.resolve();
+}
+
+function readAllFiles() {
+    return new Promise(res => {
+        glob('dist/**/!(*.min).css', (err, files) => {
+            //read files, check scopes
+            const reads = [];
+            files.forEach(file => {
+                const promise = util.read(file, data => {
+                    allFiles.push({file, data});
+                    const scope = isScoped(data);
+                    if (currentScope && scope !== currentScope) {
+                        throw 'scopes used on current css differ from file to file.';
+                    }
+                    currentScope = scope;
+                });
+                reads.push(promise);
+            });
+            Promise.all(reads).then(res);
+        });
+
+    });
+}
+
+function dispatchAction(action, currentScope) {
+
+    switch (action) {
+    case 'cleanup':
+        cleanUp(currentScope).then(store).catch(console.log);
+        break;
+    case 'replace':
+        cleanUp(currentScope).then(getNewScope).then(doScope).then(store).catch(console.log);
+        break;
+    case 'append':
+        getNewScope().then(doScope).then(store).catch(console.log);
+        break;
+    case 'nothing':
+    }
+}
+
+function startProcess() {
+
+    if (currentScope) {
+
+        const actions = ['replace', 'append', 'cleanup', 'nothing'];
+
+        const cliActions = actions.reduce((actions, action) => {
+            if (argv[action]) {
+                actions.push(action);
+            }
+            return actions;
+        }, []);
+
+        if (cliActions.length === 1) {
+            dispatchAction(cliActions[0], currentScope);
+        } else if (cliActions.length > 1) {
+            throw `multiple actions found: ${cliActions.join(', ')}`;
+        } else {
             prompt([{
                 type: 'list',
                 name: 'action',
                 message: `the css is already scoped with: '${currentScope}'. what do you want to do?`,
                 default: 'replace',
                 choices: ['replace', 'append', 'cleanup', 'nothing']
-            }]).then(res => {
-                switch (res.action) {
-                case 'cleanup':
-                    allFiles.forEach(data => cleanUp(data).then(store));
-                    break;
-                case 'replace':
-                    allFiles.forEach(data => cleanUp(data).then(store));
-                    currentScope = null;
-                    allFiles.forEach(data => doScope(data).then(store));
-                    break;
-                case 'append':
-                    allFiles.forEach(data => doScope(data).then(store));
-                    break;
-                case 'nothing':
-                }
-            });
-        } else {
-            allFiles.forEach(data => doScope(data).then(store));
+            }]).then(res => dispatchAction(res.action, currentScope));
         }
-    });
 
-});
+    } else {
 
+        dispatchAction('append');
+    }
+}
